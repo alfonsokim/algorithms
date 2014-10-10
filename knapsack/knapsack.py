c6e97d3aeb5ab0035ed5b2aacc9fe8a669e1b397
Alfonso Kim <alfonsokim84@gmail.com>
2014-10-09 19:48:01 -0500

    knapsack.py

--- knapsack/knapsack.py
+++ knapsack/knapsack.py
@@ -10,21 +10,34 @@ simple = [Item('a', 5, 10), Item('b', 4, 40), Item('c', 6, 30), Item('d', 3, 50)
 
 MAX_W = 20
 
-def knapsack_bottom_up():
-    values = []
-    for item in problema1:
-    	values.append([0 for _ in range(MAX_W)])
+# ===============================================================
+def knapsack_bottom_up(items, max_weight):
+    """
+    """
+    value_line = [0 for _ in range(max_weight + 1)]
+    values = [list(value_line) for _ in range(len(items) +  1)]
 
-    for i in range(len(problema1) + 1):
-    	item = problema1[i-1]
-    	for j in range(MAX_W + 1):
-    		if item.weight < j and (item.value + values[i - 1][j - item.weight]) > values[i - 1][j]:
-    			print item
-    			values[i][j] = item.value + values[i - 1][j - item.weight]
-    		else:
-    			values[i][j] = values[i - 1][j]
+    for i in range(1, len(items) + 1):
+        item = items[i - 1]
+        for current_max in range(max_weight + 1):
+            if item.weight > current_max:
+                values[i][current_max] = values[i - 1][current_max]
+            else:
+                a = values[i - 1][current_max]
+                b = values[i - 1][current_max - item.weight] + item.value
+                values[i][current_max] = max(a, b)
 
-    print values
+    result = []
+    current_max = max_weight
+    for i in range(len(items), 0, -1):
+        a = values[i][current_max]
+        b = values[i - 1][current_max]
+        if a != b:
+            result.append(items[i - 1])
+            current_max = items[i - 1].weight
+
+    result.reverse()
+    return result
 
 
 # ===============================================================
@@ -33,15 +46,14 @@ def knapsack_top_down_recursivo(items, n, max_w):
     """
     if n == 0:
         return 0
+    ktdr = knapsack_top_down_recursivo # Para evitar lineas largas
+    item = items[n - 1]
+    if item.weight > max_w:
+        return ktdr(items, n - 1, max_w)
     else:
-        ktdr = knapsack_top_down_recursivo # Para evitar lineas largas
-        item = items[n - 1]
-        if item.weight > max_w:
-            return ktdr(items, n - 1, max_w)
-        else:
-            a = ktdr(items, n - 1, max_w)
-            b = ktdr(items, n - 1, max_w - item.weight) + item.value
-            return max(a, b)
+        a = ktdr(items, n - 1, max_w)
+        b = ktdr(items, n - 1, max_w - item.weight) + item.value
+        return max(a, b)
 
 
 # ===============================================================
@@ -65,7 +77,12 @@ def knapsack_top_down(items, max_weight):
 
 # ===============================================================
 if __name__ == '__main__':
-    result = knapsack_top_down(simple, 10)
-    print '\n'.join([str(r) for r in result])
+    result_topdown = knapsack_top_down(problema1, 20)
+    print '%s Top Down %s' % ('*'*10, '*'*10)
+    print '\n'.join([str(r) for r in result_topdown])
+    
+    result_bottonup = knapsack_bottom_up(problema1, 20)
+    print '%s Bottom-Up %s' % ('*'*10, '*'*10)
+    print '\n'.join([str(r) for r in result_topdown])
 
 
