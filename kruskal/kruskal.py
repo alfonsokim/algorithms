17c02d077d788937547af7338942f9ba6e279104
Alfonso Kim <alfonso.kimq@gmail.com>
2017-03-07 15:52:16 -0600

    mejoras, cambio de la doc a ingles

--- kruskal/kruskal.py
+++ kruskal/kruskal.py
@@ -1,11 +1,11 @@
 
 from collections import namedtuple
 
-""" Estructura del grafo, representa una arista
+""" Graph structure
 """
 Edge = namedtuple('Edge', 'nodes, cost')
 
-""" Grafo de pruebas
+""" Just in case
 """
 test_graph = [Edge(nodes=['A', 'B'], cost=10),
               Edge(nodes=['A', 'C'], cost=15),
@@ -16,33 +16,22 @@ test_graph = [Edge(nodes=['A', 'B'], cost=10),
 
 
 # ===============================================================
-def get_minimum_edge(graph):
-    """ Encuentra la arista minima en un grafo
-        :param graph: El grafo a econtorar la arista con costo minimo
-        :return: El vertice con costo mas bajo
-    """
-    graph.sort(key=lambda(g): g.cost)
-    return graph[0]
-
-
-# ===============================================================
 def get_nodes(graph):
-    """ Encuentra todos los nombres de los vertices en un grafo
-        :param graph: El grafo a encontrar sus vertices
-        :return: El nombre de los nodos en el grafo
+    """ Returns the name of all the vertices in the graph.
+        Could be inlined in the code, was added for clarity.
+        :param graph: The graph to read
+        :return: The name of the vertices
     """
-    nodes = set()
-    for edge in graph:
-        [nodes.add(v) for v in edge.nodes]
-    return nodes
+    return set([v for edge in graph for v in edge.nodes])
 
 
 # ===============================================================
 def find_partition(partitions, node):
-    """ Encuentra la particion donde se encuentra un nodo
-        :param partitions:  La lista de particiones a buscar
-        :param node:    El nodo a buscar en la lista
-        :return:        La particion en la que se encuentra el nodo
+    """ Finds the partition of the graph where node is.
+        Could be inlined in the code, was added for clarity.
+        :param partitions: The list of partitions 
+        :param node: The node to look for
+        :return: The partition where the node is
     """
     for partition in partitions:
         if node in partition:
@@ -60,46 +49,45 @@ def kruskal(graph):
     partitions = [[v] for v in get_nodes(graph)]
     minimum_spanning_tree = []
     while copy and partitions:
-        min_edge = get_minimum_edge(copy)
-        nodes = min_edge.nodes
+        minimum_edge = sorted(copy, key=lambda g: g.cost)[0]
+        nodes = minimum_edge.nodes
         partition0 = find_partition(partitions, nodes[0])
         partition1 = find_partition(partitions, nodes[1])
         if sorted(partition0) != sorted(partition1):
             partition0.extend(partition1)
             partitions.remove(partition1)
-            minimum_spanning_tree.append(min_edge)
-        copy.remove(min_edge)
+            minimum_spanning_tree.append(minimum_edge)
+        copy.remove(minimum_edge)
     return minimum_spanning_tree
 
 
 # ===============================================================
-def read_graph_file(a_file):
-    """ Lee un archivo en el formato a,b,c donde a y b son 
-        los nombres del vertice y c es el costo de la arista 
-        entre ellos.
-        :param a_file: El nombre del archivo a leer
-        :return: El grafo parseado
+def read_graph_file(file_name):
+    """ Utilitary method. This is not part of the algorithm.
+        Reads a file where each line has the format a,b,c. 
+        a and b are the vertex names, c is the cost of the edge between them.
+        :param file_name: The name of the file to read
+        :return: The list of the edges of the graph
     """
-    inf = open(a_file, 'r')
-    graph = []
-    for c, line in enumerate(inf):
+    def _line_to_edge(index, line):
+        """ Parse each line """
         items = line.upper().strip().split(',')
-        if len(items) != 3: raise Error('Error de formato en linea %i: %s' % (c , line))
-        graph.append(Edge(nodes=sorted(items[:2]), cost=int(items[2])))
+        if len(items) != 3: 
+            raise Exception('Invalid line %i: %s' % (index, line))
+        return Edge(nodes=items[:2], cost=int(items[2]))
+    # -----------------------------------------------------------
+    graph_data = open(file_name, 'r')
+    graph = [_line_to_edge(c, line) for c, line in enumerate(graph_data)]
+    graph_data.close()
     return graph
 
 
 # ===============================================================
 if __name__ == '__main__':
-    """ Punto de entrada de la consola
+    """ Main entry point
+        Reads the test file and check the result of the algorithm
     """
     minimum_spanning_tree = kruskal(read_graph_file('graph.txt'))
-    solution = read_graph_file('solution.txt')
-    by_edges = lambda(e) : ''.join(e.nodes)
-    minimum_spanning_tree.sort(key=by_edges)
-    solution.sort(key=by_edges)
-    if minimum_spanning_tree != solution:
-        for g, s in zip(minimum_spanning_tree, solution):
-            print 'g: %s, s:%s' % (g, s)
-    else:
-        print 'OK!'
+    path = '->'.join(['%s%s' % tuple(n.nodes) for n in minimum_spanning_tree])
+    assert path == 'BC->KL->OR->EF->KP->OP->OS->RV->IM->AC->LM->WX->DE->CF->DG->FH->QT->JO->KQ->GJ->TY->NU->SX->UY->YZ'
+    print 'OK!'
